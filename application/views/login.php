fc29b73d21b32617142d1d31784fe3ee411938b7
yinzhixiaxue <775288786@qq.com>
2017-04-20 19:45:48 +0800

    login

--- application/views/login.php
+++ application/views/login.php
@@ -3,47 +3,36 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title></title>	
-		<link rel="stylesheet" href="../assets/css/core.css" />
-		<link rel="stylesheet" href="../assets/css/menu.css" />
-		<link rel="stylesheet" href="../assets/css/amazeui.css" />
-		<link rel="stylesheet" href="../assets/css/component.css" />
-		<link rel="stylesheet" href="../assets/css/page/form.css" />
+		<title></title>
+		<base href="<?php echo site_url()?>">
+		<link rel="stylesheet" href="assets/css/core.css" />
+		<link rel="stylesheet" href="assets/css/menu.css" />
+		<link rel="stylesheet" href="assets/css/amazeui.css" />
+		<link rel="stylesheet" href="assets/css/component.css" />
+		<link rel="stylesheet" href="assets/css/page/form.css" />
 	</head>
 	<body>
 		<div class="account-pages">
 			<div class="wrapper-page">
 				<div class="text-center">
-	                <a href="index.html" class="logo"><span>Admin<span>to</span></span></a>
+	                <a href="index.html" class="logo"><span>社区<span>矫正系统</span></span></a>
 	            </div>
 	            
 	            <div class="m-t-40 card-box">
-	            	<div class="text-center">
-	                    <h4 class="text-uppercase font-bold m-b-0">Sign In</h4>
-	                </div>
 	                <div class="panel-body">
-	                	<form class="am-form">
+	                	<form class="am-form" action="welcome/check_login" method="post">
 	                		<div class="am-g">
 	                			<div class="am-form-group">
-							      <input type="email" class="am-radius"  placeholder="Username">
+							      <input type="text" id="username" class="am-radius" name="username" placeholder="用户名">
 							    </div>
 							
 							    <div class="am-form-group form-horizontal m-t-20">
-							      <input type="password" class="am-radius"  placeholder="Password">
+							      <input type="password" id="password" class="am-radius" name="password" placeholder="密码">
 							    </div>
-							    
-							    <div class="am-form-group ">
-		                           	<label style="font-weight: normal;color: #999;">
-								        <input type="checkbox" class="remeber"> Remember me
-								    </label>
-		                        </div>
-		                        
+
+		              
 		                        <div class="am-form-group ">
-		                        	<button type="button" class="am-btn am-btn-primary am-radius" style="width: 100%;height: 100%;">Log In</button>
-		                        </div>
-		                        
-		                        <div class="am-form-group ">
-		                        <a href="page-recoverpw.html" class="text-muted"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
+		                        	<button type="submit" class="am-btn am-btn-primary am-radius" style="width: 100%;height: 100%;">登录</button>
 		                        </div>
 	                		</div>
 
@@ -53,5 +42,17 @@
 	            </div>
 			</div>
 		</div>
+<!-- 		<script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script> -->
+		<script src="assets/js/jquery-2.1.0.js"></script>
+		<script src='assets/js/form.js'></script>
+		<script>
+			$username=$('#username');
+			$username.reg=/^[a-z]+$/;
+			$password=$('#password');
+			$password.reg=/^[a-zA-Z0-9]+$/;
+			let arr=[$username,$password];
+			let form=new Form(arr);
+			form.listen();
+		</script>
 	</body>
 </html>
